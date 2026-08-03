59d3f7b7ed4a32e708e0a861175bfd739f8db527
massah <arafeh.massah2007@gmail.com>
2026-08-03 12:23:21 -0400

    change of font

--- server/src/app/layout.tsx
+++ server/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({
+const dmSans = DM_Sans({
   subsets: ["latin"],
 });
 
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} antialiased`}
+        className={`${dmSans.className} antialiased`}
       >
         {children}
       </body>
