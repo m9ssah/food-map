916fdcf2290956e0140df791517a6b39752b67b4
massah <arafeh.massah2007@gmail.com>
2026-09-01 01:59:12 -0400

    password

--- server/src/app/login/page.tsx
+++ server/src/app/login/page.tsx
@@ -156,6 +156,7 @@ export default function LoginPage() {
               placeholder="Enter your password"
               className="w-full px-4 py-2 bg-[#17283C] text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
               required
+              minLength={6}
             />
           </div>
 
